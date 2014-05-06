dcf14e90714df97b1c4d7c16c16bd640e2f4d5e6
Marcello Palmitessa <marcello.palmitessa@avanade.com>
2014-05-06 09:18:11 +0200

    wip

---
+++ demos/demo-debugger/server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// server.js
+var x = 5;
+
+setTimeout(function () {
+   debugger;
+   console.log("world");
+}, 1000);
+
+console.log("hello");
--- demos/demo3/server.js
+++ demos/demo3/server.js
@@ -1,9 +1,5 @@
-﻿//var http = require('http');
-//var port = process.env.port || 1337;
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
+﻿"use strict";
+
 var sys = require( "sys" ),
    http = require( "http" ),
    url = require( "url" ),
